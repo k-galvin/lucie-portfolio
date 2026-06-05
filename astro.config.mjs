aa06352498a5f5217b801d53c740c79d2cc6147d
k-galvin <110426866+k-galvin@users.noreply.github.com>
2026-06-05 11:02:06 -0700

    Improve SEO for artist name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://luciegalvin.com',
+  integrations: [
+    sitemap({
+      filter: (page) => page !== 'https://luciegalvin.com/admin/'
+    })
+  ]
+});
